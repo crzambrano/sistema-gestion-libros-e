f50cf617e188bb16603c14cd1c6592784b11bac6
crzambrano <crzambranoce@uide.edu.ec>
2024-12-21 16:23:12 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,11 +8,6 @@ import (
 func main() {
 	fmt.Println("Sistema de Gestión de Libros Electrónicos - Servidor iniciado")
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/books", booksHandler)
 	http.ListenAndServe(":8080", nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"mensaje": "Bienvenido al Sistema de Gestión de Libros Electrónicos"}`))
 }
