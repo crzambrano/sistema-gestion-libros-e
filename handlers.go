f437e6c76ea0a70491a92b893df6bc2cc4724ae0
crzambrano <crzambranoce@uide.edu.ec>
2024-12-21 16:30:46 -0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -34,3 +34,26 @@ func addBookHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newBook)
 }
+
+import "fmt"
+
+// Buscar un libro por ID
+func getBookHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "Falta el parámetro 'id'", http.StatusBadRequest)
+		return
+	}
+
+	for _, book := range library {
+		if book.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
+	err := &BookNotFoundError{BookID: id}
+	http.Error(w, err.Error(), http.StatusNotFound)
+}
