14c626a889bdf1632ba771bbe473bd4020796742
crzambrano <crzambranoce@uide.edu.ec>
2024-12-21 16:04:58 -0500

    Create user.go

---
+++ pkg/books/pkg/users/user.go
@@ -0,0 +1,17 @@
+package users
+
+type User struct {
+	ID       string
+	Name     string
+	Email    string
+	Password string
+}
+
+func NewUser(id, name, email, password string) *User {
+	return &User{
+		ID:       id,
+		Name:     name,
+		Email:    email,
+		Password: password,
+	}
+}
